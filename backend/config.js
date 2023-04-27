26533f1a799de77c5551971490271bd7eedd5926
Ali Sinan Cetingoz <sinan.cetingoz@yemeksepeti.com>
2023-04-27 15:54:46 +0300

    google's firebase tutorial

--- backend/config.js
+++ backend/config.js
@@ -1,19 +1,27 @@
-const firebase = require('firebase');
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyADSrtgLp139Xg42PCzG0YoX5ZfAQloze8",
-    authDomain: "cs308-group20.firebaseapp.com",
-    projectId: "cs308-group20",
-    storageBucket: "cs308-group20.appspot.com",
-    messagingSenderId: "587594797393",
-    appId: "1:587594797393:web:769bb73e1d57652a480370",
-    measurementId: "G-QLGPBFHL11"
-  };
+  apiKey: "AIzaSyADSrtgLp139Xg42PCzG0YoX5ZfAQloze8",
+  authDomain: "cs308-group20.firebaseapp.com",
+  projectId: "cs308-group20",
+  storageBucket: "cs308-group20.appspot.com",
+  messagingSenderId: "587594797393",
+  appId: "1:587594797393:web:769bb73e1d57652a480370",
+  measurementId: "G-QLGPBFHL11"
+};
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-
-const Customer = db.collection("customers");
-module.exports = Customer;
-
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 //https://www.youtube.com/watch?v=YPsftzOURLw&ab_channel=SyedZano 9. dakikadan devam
+
+const firestore = getFirestore();
+
+console.log('Hello firestore');
