40e2e1c783622734c31bb008c24ee7430b7886d6
Sinan Cetingoz <alisinan@sabanciuniv.edu>
2023-05-01 16:02:08 +0300

    added order mechanisms

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.options("*", cors());
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
 app.use(authJwt());
+
 // app.use((err, req, res, next) => {
 //     if(err){
 //         res.status(500).json({message: "error in the server"})
