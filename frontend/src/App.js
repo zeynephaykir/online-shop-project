acfa9b421ec77b37a97a320f55bd96e25c964d1a
Sinan Cetingoz <alisinan@sabanciuniv.edu>
2023-05-04 02:02:20 +0300

    started MERN stack app

---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+import logo from "./logo.svg";
+import "./App.css";
+import data from "./data";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Crochet By Sarin</a>
+      </header>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name} </p>
+                </a>
+                <p>
+                  <strong>${product.price} </strong>
+                </p>
+                <button>Add To Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,61 @@
+const data = {
+    products: [
+        {
+            name: 'Rainbow Floral Crochet Vest',
+            slug: 'rainbow-vest',
+            category: 'Vests',
+            image: '/images/p1.jpg',
+            price: 100,
+            countInStock: 5,
+            brand:'Crochet by Sarin',
+            rating: 4.6,
+            numReviews: 10
+        },
+        {
+            name: 'Brown Floral Crochet Top',
+            slug: 'brown-floral-top',
+            category: 'Crochet Tops',
+            image: '/images/p2.jpg',
+            price: 120,
+            countInStock: 3,
+            brand:'Crochet by Sarin',
+            rating: 4.0,
+            numReviews: 4
+        },
+        {
+            name: 'Blue Crochet Bucket Hat',
+            slug: 'bucket-hat',
+            category: 'Hats',
+            image: '/images/p3.jpg',
+            price: 70,
+            countInStock: 6,
+            brand:'Crochet by Sarin',
+            rating: 5,
+            numReviews: 2
+        },
+        {
+            name: 'Crochet Crop Top',
+            slug: 'crochet-crop-top',
+            category: 'Crochet Tops',
+            image: '/images/p4.jpg',
+            price: 100,
+            countInStock: 3,
+            brand:'Crochet by Sarin',
+            rating: 4.3,
+            numReviews: 7
+        },
+        {
+            name: 'Ecru Floral Crochet Cardigan',
+            slug: 'ecru-floral-cardigan',
+            category: 'Cardigans',
+            image: '/images/p5.jpg',
+            price: 150,
+            countInStock: 1,
+            brand:'Crochet by Sarin',
+            rating: 4.1,
+            numReviews: 5
+        },
+    ]
+}
+
+export default data;
