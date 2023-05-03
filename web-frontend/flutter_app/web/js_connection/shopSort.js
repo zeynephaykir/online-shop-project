996dd00e2842408b2ed8f59605077f01ef7acca2
Rana Islek <ranaislek@sabanciuniv.edu>
2023-05-03 22:03:46 +0300

    shopping page features have been fixed

--- web-frontend/flutter_app/web/js_connection/shopSort.js
+++ web-frontend/flutter_app/web/js_connection/shopSort.js
@@ -43,8 +43,7 @@ function fetchAndRenderProducts(sortOption) {
             <img src="${product.image}" class="img-fluid" alt="Image">
             <div class="mask-icon">
               <ul>
-                <li><a href="#" data-toggle="tooltip" data-placement="right" title="View"><i class="fas fa-eye"></i></a></li>
-                <li><a href="#" data-toggle="tooltip" data-placement="right" title="Compare"><i class="fas fa-sync-alt"></i></a></li>
+                <li><a href="shop-detail.html" data-toggle="tooltip" data-placement="right" title="View"><i class="fas fa-eye"></i></a></li>
                 <li><a href="#" data-toggle="tooltip" data-placement="right" title="Add to Wishlist"><i class="far fa-heart"></i></a></li>
               </ul>
               <a class="cart" href="#">Add to Cart</a>
