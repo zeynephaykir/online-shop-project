9c448b7ef99149b3dc0d46244183eb4bf6e39961
Zeynep Haykır <zeynephaykir@sabanciuniv.edu>
2023-05-04 16:34:17 +0300

    product & rating component initiliazed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,23 @@ import "./App.css";
 import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import {LinkContainer} from 'react-router-bootstrap';
+
 function App() {
   return (
     <BrowserRouter>
       <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">Crochet By Sarin</Link>
+          <Navbar bg="success" variant="dark"> 
+            <Container>
+                <LinkContainer to ="/">
+                 <Navbar.Brand>Crochet By Sarin</Navbar.Brand>
+                </LinkContainer>
+            </Container>
+          </Navbar>
+          
         </header>
         <main>
           <Routes>
