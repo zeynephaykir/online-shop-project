6a87404488b28d62e8de047c6c1dc800fbb40b91
Rana Islek <ranaislek@sabanciuniv.edu>
2023-05-04 22:31:26 +0300

    sign-in screen has been created and feature update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { useContext } from 'react';
 import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SigninScreen';
 
 function App() {
   const { state } = useContext(Store);
@@ -17,10 +18,10 @@ function App() {
     <BrowserRouter>
       <div className="d-flex flex-column site-container">
         <header>
-          <Navbar bg="dark" variant="dark">
+          <Navbar style={{ backgroundColor: '#ffe9c9' }} variant="light">
             <Container>
               <LinkContainer to="/">
-                <Navbar.Brand>amazona</Navbar.Brand>
+                <Navbar.Brand style={{ color:'#ff5757' }}>Crochet By Sarin</Navbar.Brand>
               </LinkContainer>
               <Nav className="me-auto">
                 <Link to="/cart" className="nav-link">
@@ -40,6 +41,7 @@ function App() {
             <Routes>
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/cart" element={<CartScreen />} />
+              <Route path="/signin" element={<SigninScreen />} />
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -45,9 +45,9 @@ function HomeScreen() {
   return (
     <div>
       <Helmet>
-        <title>Crochet by Sarin</title>
+        <title >Crochet by Sarin</title>
       </Helmet>
-      <h1>Featured Products</h1>
+      <h1 style={{ color:'#ff5757' }}>Featured Products</h1>
       <div className="products">
         {loading ? (
           <LoadingBox />
