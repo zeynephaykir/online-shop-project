40fb4ef21c500311953660a1e4bc7f6ed829c590
Rana Islek <ranaislek@sabanciuniv.edu>
2023-05-05 15:42:56 +0300

    cloudinary api has been created and added

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import seedRouter from "./routes/seedRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import userRouter from './routes/userRoutes.js';
 import orderRouter from './routes/orderRoutes.js';
+import uploadRouter from './routes/uploadRoutes.js';
 
 dotenv.config();
 
@@ -24,6 +25,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/upload', uploadRouter);
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter)
 app.use('/api/users', userRouter);
