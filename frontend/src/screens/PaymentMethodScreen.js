7444632d5c0e9a36fd74796fa2c342dcc29d5036
egeoztas <egeoztas@sabanciuniv.edu>
2023-06-10 20:12:17 +0300

    Added card information

--- frontend/src/screens/PaymentMethodScreen.js
+++ frontend/src/screens/PaymentMethodScreen.js
@@ -16,18 +16,55 @@ export default function PaymentMethodScreen() {
   const [paymentMethodName, setPaymentMethod] = useState(
     paymentMethod || 'CreditCard'
   );
+  
+  const [cardInfo, setCardInfo] = useState({
+    cardNumber: '',
+    expiryDate: '',
+    cvv: '',
+  });
 
   useEffect(() => {
     if (!shippingAddress.address) {
       navigate('/shipping');
     }
   }, [shippingAddress, navigate]);
+
+  const handleCardInfoChange = (e) => {
+    setCardInfo({
+      ...cardInfo,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   const submitHandler = (e) => {
     e.preventDefault();
+  
+    // Card number validation
+    if (!/^\d{16}$/.test(cardInfo.cardNumber)) {
+      alert("Please enter a valid card number.");
+      return;
+    }
+  
+    // Expiry date validation
+    if (!/^(0[1-9]|1[0-2])\/\d{2}$/.test(cardInfo.expiryDate)) {
+      alert("Please enter a valid expiry date.");
+      return;
+    }
+  
+    // CVV validation
+    if (!/^\d{3}$/.test(cardInfo.cvv)) {
+      alert("Please enter a valid CVV.");
+      return;
+    }
+  
     ctxDispatch({ type: 'SAVE_PAYMENT_METHOD', payload: paymentMethodName });
+    ctxDispatch({ type: 'SAVE_CARD_INFO', payload: cardInfo });
     localStorage.setItem('paymentMethod', paymentMethodName);
+    localStorage.setItem('cardInfo', JSON.stringify(cardInfo));
     navigate('/placeorder');
   };
+  
+
   return (
     <div>
       <CheckoutSteps step1 step2 step3></CheckoutSteps>
@@ -46,6 +83,43 @@ export default function PaymentMethodScreen() {
               checked={paymentMethodName === 'CreditCard'}
               onChange={(e) => setPaymentMethod(e.target.value)}
             />
+            {paymentMethodName === 'CreditCard' && (
+              <>
+                <Form.Group className="mb-3">
+                  <Form.Label>Card Number</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="cardNumber"
+                    value={cardInfo.cardNumber}
+                    onChange={handleCardInfoChange}
+                    placeholder="Card Number"
+                    required
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>Expiry Date</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="expiryDate"
+                    value={cardInfo.expiryDate}
+                    onChange={handleCardInfoChange}
+                    placeholder="MM/YY"
+                    required
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>CVV</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="cvv"
+                    value={cardInfo.cvv}
+                    onChange={handleCardInfoChange}
+                    placeholder="CVV"
+                    required
+                  />
+                </Form.Group>
+              </>
+            )}
           </div>
           <div className="mb-3">
             <Form.Check
