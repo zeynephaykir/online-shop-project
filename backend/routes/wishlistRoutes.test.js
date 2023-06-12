85cd1ae35029d85aafb95dca5cf4ba63471deb74
Zeynep Haykır <zeynephaykir@sabanciuniv.edu>
2023-06-12 05:00:29 +0300

    wishlist unit test cases

---
+++ backend/routes/wishlistRoutes.test.js
@@ -0,0 +1,10 @@
+import express from "express";
+import expressAsyncHandler from 'express-async-handler';
+import Wishlist from "../models/wishlistModel.js";
+import { isAuth } from '../utils.js';
+import  from "./wishlistRoutes";
+
+jest.mock("express");
+jest.mock('express-async-handler');
+jest.mock("../models/wishlistModel.js");
+jest.mock('../utils.js');
